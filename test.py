d222a6942a3b22f2e9a6f87d483995590b3f5382
bkc17 <bharat.kumar6968@gmail.com>
2021-01-14 03:04:44 +0100

    Finished making the game in the terminal

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-for _ in range(4, 6):
-    print("5", end = " ")
+def hello():
+    print("hello1")
+    return True
 
-l = [0, 1, 2, 3]
-for i in range(len(l)):
-    print(i)
+if hello():
+    print("hello2")
