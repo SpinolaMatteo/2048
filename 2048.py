18f0c301ff59c648832df1904ac7ff86c3c7bf61
bkc17 <bharat.kumar6968@gmail.com>
2021-01-14 02:15:50 +0100

    Added a function that moves the whole grid left

--- 2048.py
+++ 2048.py
@@ -1,14 +1,13 @@
 
 #Functions
 
-#grid =[] #remove later!!!!!!!!!!!!!!!!!!!!!!!!
 size = 5
 def startGame():
     index = 0
     grid = []
     for _ in range(size**2):
         if (index < (size**2)-1):
-            grid.append(0)
+            grid.append(2)
         else:
             grid.append(2)
         index = index+1
@@ -49,6 +48,7 @@ def empty(row, low_limit, high_limit):
             count = count+1
     return count
 
+#arranges all the empty spaces on the right side and all the numbers on the left
 def arrange(grid, low_limit, high_limit):
     numZero = empty(grid, low_limit, high_limit-1)
     count1 = 0
@@ -59,14 +59,40 @@ def arrange(grid, low_limit, high_limit):
                 grid[i+1] = 0
         count1 = count1+1
 
+def move_row_left(grid, low_limit, high_limit):
+    grid_copy = grid[:]
+    arrange(grid, low_limit, high_limit)
 
+    for i in range(low_limit, high_limit-1):
+        if (grid[i] == grid[i+1]) and (grid[i] != 0):
+            grid[i] = 2*grid[i]
+            grid[i+1] = 0
+    
+    arrange(grid, low_limit, high_limit)
+    if(grid_copy == grid):
+        return False
+    return True
 
-#def moveLeft(grid):
-
+#moves the whole grid left
+def moveLeft(grid):
+    c = False
+    changed = False
+    #I use "changed" to determine if the grid changed after a move to the left or if the move did not do anything
+    for i in range(size):
+        low_limit = i*size
+        high_limit = low_limit + size
+        c = move_row_left(grid, low_limit, high_limit)
+        if(c):
+            changed = True
+    return changed
 
 #Main code
 
 grid = startGame()
 printGrid(grid)
+
+moveLeft(grid)
+printGrid(grid)
+
 
 
--- test.py
+++ test.py
@@ -2,4 +2,5 @@ for _ in range(4, 6):
     print("5", end = " ")
 
 l = [0, 1, 2, 3]
-print(l[0:2])
+for i in range(len(l)):
+    print(i)
